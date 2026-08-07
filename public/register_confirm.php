51d867bdc9b8ec7af902c0d3219aecfdfa4d9ce1
Kouhei Yagi <artoflife19870310@gmail.com>
2026-08-08 05:40:40 +0900

    refactor: [public/register_confirm.php] 会員登録処理に例外処理を追加

--- public/register_confirm.php
+++ public/register_confirm.php
@@ -12,22 +12,34 @@ $password = $_POST['password'];
 $dsn = 'mysql:host=localhost;dbname=shop;charset=utf8mb4';
 $username = 'staff';
 $dbPassword = 'password';
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
 
-// データベース接続
-$pdo = new PDO($dsn, $username, $dbPassword);
+// 例外処理
+try {
+    // データベース接続
+    $pdo = new PDO($dsn, $username, $dbPassword, $options);
 
-// クエリ準備
-$sql = 'INSERT INTO customer(name, address, login, password) VALUES(:name, :address, :login, :password)';
-$stmt = $pdo->prepare($sql);
+    // クエリ準備
+    $sql = 'INSERT INTO customer(name, address, login, password) VALUES(:name, :address, :login, :password)';
+    $stmt = $pdo->prepare($sql);
 
-// パラメータ設定
-$stmt->bindValue(':name', $name, PDO::PARAM_STR);
-$stmt->bindValue(':address', $address, PDO::PARAM_STR);
-$stmt->bindValue(':login', $login, PDO::PARAM_STR);
-$stmt->bindValue(':password', $password, PDO::PARAM_STR);
+    // パラメータ設定
+    $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+    $stmt->bindValue(':address', $address, PDO::PARAM_STR);
+    $stmt->bindValue(':login', $login, PDO::PARAM_STR);
+    $stmt->bindValue(':password', $password, PDO::PARAM_STR);
 
-// クエリ実行
-$stmt->execute();
+    // クエリ実行
+    $stmt->execute();
+
+    // 例外発生時処理
+} catch (PDOException $e) {
+    error_log($e->getMessage());
+    exit('システムエラーが発生しました。');
+}
 
 // ＜出力＞
 echo '登録しました。';
