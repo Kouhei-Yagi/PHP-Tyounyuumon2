59f6614e716ed3ce971f367e3c622c0487a459e6
Kouhei Yagi <artoflife19870310@gmail.com>
2026-08-11 06:32:15 +0900

    refactor: [public/register_confirm.php] DB接続処理を関数呼び出しに変更

--- public/register_confirm.php
+++ public/register_confirm.php
@@ -1,4 +1,7 @@
 <?php
+// 関数ファイルの読み込み
+require_once(__DIR__ . '/../app/db.php');
+
 // セッション開始
 session_start();
 
@@ -71,15 +74,11 @@ foreach ($rules as $key => $max) {
 $dsn = 'mysql:host=localhost;dbname=shop;charset=utf8mb4';
 $username = 'staff';
 $dbPassword = 'password';
-$options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_EMULATE_PREPARES => false,
-];
 
 // 例外処理
 try {
     // データベース接続
-    $pdo = new PDO($dsn, $username, $dbPassword, $options);
+    $pdo = getDbConnection($dsn, $username, $dbPassword);
 
     // ログイン名重複チェック
     // クエリ準備
