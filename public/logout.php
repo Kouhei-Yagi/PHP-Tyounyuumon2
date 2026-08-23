ac8b49ed5889143f537e3a6c35213e2f91cc3b52
Kouhei Yagi <artoflife19870310@gmail.com>
2026-08-24 06:36:34 +0900

    feat: [public/logout.php] ログアウト画面を追加

---
+++ public/logout.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>ログアウト</title>
+</head>
+
+<body>
+    <p>ログアウトしますか？</p>
+
+    <form action="logout_confirm.php" method="post">
+        <button type="submit">ログアウト</button>
+    </form>
+</body>
+
+</html>
