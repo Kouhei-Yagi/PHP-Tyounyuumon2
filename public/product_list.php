bcbdc8f427c0cc8a8577b9a197a6157460ccf067
Kouhei Yagi <artoflife19870310@gmail.com>
2026-08-30 06:18:23 +0900

    refactor: [public/product_list.php] DB接続処理を関数呼び出しに変更

--- public/product_list.php
+++ public/product_list.php
@@ -1,20 +1,15 @@
 <?php
-// ＜処理＞
-// 商品一覧取得
-// データベース接続設定
-$dsn = 'mysql:host=localhost;dbname=shop;charset=utf8mb4';
-$user = 'staff';
-$password = 'password';
-$options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_EMULATE_PREPARES => false,
-];
+// ＜前処理＞
+// 関数ファイルの読み込み
+require_once(__DIR__ . '/../app/db.php');
 
+// ＜処理＞
 // 例外処理
 try {
     // データベース接続
-    $pdo = new PDO($dsn, $user, $password, $options);
+    $pdo = getDbConnection();
 
+    // 商品一覧取得
     // クエリ実行
     $sqlSelectProducts = 'SELECT * FROM product';
     $stmtSelectProducts = $pdo->query($sqlSelectProducts);
