026eba1a9ccb0f45c207bff2e8076e79e6aa0b56
Kouhei Yagi <artoflife19870310@gmail.com>
2026-09-04 06:08:53 +0900

    refactor: [app/product.php] 商品1件取得処理を関数化して関数呼び出しに変更

--- public/product_detail.php
+++ public/product_detail.php
@@ -1,8 +1,8 @@
 <?php
 // ＜前処理＞
 // 関数ファイルの読み込み
-require_once(__DIR__ . '/../app/db.php');
 require_once(__DIR__ . '/../app/validation.php');
+require_once(__DIR__ . '/../app/product.php');
 
 // ＜入力＞
 // クエリパラメータ取得
@@ -22,22 +22,8 @@ $idInt = (int)$id;
 // ＜処理＞
 // 例外処理
 try {
-    // データベース接続
-    $pdo = getDbConnection();
-
-    // 商品詳細取得
-    // クエリ準備
-    $sqlSelectProduct = 'SELECT * FROM product WHERE id = :id';
-    $stmtSelectProduct = $pdo->prepare($sqlSelectProduct);
-
-    // パラメータ設定
-    $stmtSelectProduct->bindValue(':id', $idInt, PDO::PARAM_INT);
-
-    // クエリ実行
-    $stmtSelectProduct->execute();
-
-    // 結果取得
-    $product = $stmtSelectProduct->fetch(PDO::FETCH_ASSOC);
+    // 商品1件取得
+    $product = getProduct($idInt);
 
     // 商品存在チェック
     if (!$product) {
