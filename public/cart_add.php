168753083635b6619aa5e8fab218a1cb35ce10ad
Kouhei Yagi <artoflife19870310@gmail.com>
2026-09-09 08:29:14 +0900

    feat: [public/cart_add.php] 同一商品のカート登録時にUPDATEへ切り替える処理を追加

--- public/cart_add.php
+++ public/cart_add.php
@@ -15,8 +15,8 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 }
 
 // 送信・選択値取得
-$csrfToken = filter_input(INPUT_POST, 'csrf_token');
 $customerId = $_SESSION['auth']['id'];
+$csrfToken = filter_input(INPUT_POST, 'csrf_token');
 $productId = filter_input(INPUT_POST, 'id');
 $count = filter_input(INPUT_POST, 'count');
 
@@ -48,14 +48,14 @@ if (!ctype_digit($productId) || !ctype_digit($count)) {
     exit('不正なアクセスです。');
 }
 
-// 選択値範囲チェック
-if ($count < 1 || $count > 10) {
-    exit('不正なアクセスです。');
-}
-
 // 商品IDと個数を整数型に変換
 $productIdInt = (int)$productId;
 $countInt = (int)$count;
+
+// 選択値範囲チェック
+if ($countInt < 1 || $countInt > 10) {
+    exit('不正なアクセスです。');
+}
 
 // ＜処理＞
 // データベース接続設定
@@ -72,18 +72,49 @@ try {
     // データベース接続
     $pdo = new PDO($dsn, $user, $dbPassword, $options);
 
-    // カート登録
+    // カート情報取得
     // クエリ準備
-    $sqlInsertCart = 'INSERT INTO cart(customer_id, product_id, count) VALUES (:customer_id, :product_id, :count)';
-    $stmtInsertCart = $pdo->prepare($sqlInsertCart);
+    $sqlSelectCart = 'SELECT * FROM cart WHERE customer_id = :customer_id AND product_id = :product_id';
+    $stmtSelectCart = $pdo->prepare($sqlSelectCart);
 
-    // クエリパラメータ設定
-    $stmtInsertCart->bindValue('customer_id', $customerId, PDO::PARAM_INT);
-    $stmtInsertCart->bindValue('product_id', $productIdInt, PDO::PARAM_INT);
-    $stmtInsertCart->bindValue('count', $countInt, PDO::PARAM_INT);
+    // パラメータ設定
+    $stmtSelectCart->bindValue(':customer_id', $customerId, PDO::PARAM_INT);
+    $stmtSelectCart->bindValue(':product_id', $productIdInt, PDO::PARAM_INT);
 
     // クエリ実行
-    $stmtInsertCart->execute();
+    $stmtSelectCart->execute();
+
+    // 結果取得
+    $cartItem = $stmtSelectCart->fetch(PDO::FETCH_ASSOC);
+
+    // 既に同じ商品があればカート更新・なければカート登録
+    if ($cartItem) {
+        // カート更新
+        // クエリ準備
+        $sqlUpdateCart = 'UPDATE cart SET count = :count WHERE customer_id = :customer_id AND product_id = :product_id';
+        $stmtUpdateCart = $pdo->prepare($sqlUpdateCart);
+
+        // パラメータ設定
+        $stmtUpdateCart->bindValue(':count', $countInt, PDO::PARAM_INT);
+        $stmtUpdateCart->bindValue(':customer_id', $customerId, PDO::PARAM_INT);
+        $stmtUpdateCart->bindValue(':product_id', $productIdInt, PDO::PARAM_INT);
+
+        // クエリ実行
+        $stmtUpdateCart->execute();
+    } else {
+        // カート登録
+        // クエリ準備
+        $sqlInsertCart = 'INSERT INTO cart(customer_id, product_id, count) VALUES (:customer_id, :product_id, :count)';
+        $stmtInsertCart = $pdo->prepare($sqlInsertCart);
+
+        // クエリパラメータ設定
+        $stmtInsertCart->bindValue('customer_id', $customerId, PDO::PARAM_INT);
+        $stmtInsertCart->bindValue('product_id', $productIdInt, PDO::PARAM_INT);
+        $stmtInsertCart->bindValue('count', $countInt, PDO::PARAM_INT);
+
+        // クエリ実行
+        $stmtInsertCart->execute();
+    }
 
     // 例外発生時処理
 } catch (PDOException $e) {
@@ -92,4 +123,8 @@ try {
 }
 
 // ＜出力＞
-echo 'カートに登録しました。';
+if ($cartItem) {
+    echo 'カート情報を更新しました。';
+} else {
+    echo 'カートに商品を登録しました。';
+}
