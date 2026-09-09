322d91345d6ebd77cd9af9a9f89bfc273a034d13
Kouhei Yagi <artoflife19870310@gmail.com>
2026-09-10 05:51:12 +0900

    refactor: [public/cart_add.php] DB接続処理を関数呼び出しに変更

--- public/cart_add.php
+++ public/cart_add.php
@@ -1,5 +1,8 @@
 <?php
 // ＜前処理＞
+// 関数ファイルの読み込み
+require_once(__DIR__ . '/../app/db.php');
+
 // セッション開始
 session_start();
 
@@ -58,19 +61,10 @@ if ($countInt < 1 || $countInt > 10) {
 }
 
 // ＜処理＞
-// データベース接続設定
-$dsn = 'mysql:host=localhost;dbname=shop;charset=utf8mb4';
-$user = 'staff';
-$dbPassword = 'password';
-$options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_EMULATE_PREPARES => false,
-];
-
 // 例外処理
 try {
     // データベース接続
-    $pdo = new PDO($dsn, $user, $dbPassword, $options);
+    $pdo = getDbConnection();
 
     // カート情報取得
     // クエリ準備
