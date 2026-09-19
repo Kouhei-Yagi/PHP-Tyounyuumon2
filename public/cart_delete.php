284dd3bdb6d4e388d4a93a54cc9546ab8027e0ba
Kouhei Yagi <artoflife19870310@gmail.com>
2026-09-20 06:05:59 +0900

    refactor: [public/cart_delete.php] DB接続処理を関数呼び出しに変更

--- public/cart_delete.php
+++ public/cart_delete.php
@@ -8,6 +8,9 @@
 // テーブル：cartテーブル
 
 // ＜前処理＞
+// 関数ファイルの読み込み
+require_once(__DIR__ . '/../app/db.php');
+
 // セッション開始
 session_start();
 
@@ -59,21 +62,12 @@ if (!ctype_digit($productId)) {
 $productIdInt = (int)$productId;
 
 // ＜処理＞
-// カート商品削除
-// データベース接続設定
-$dsn = 'mysql:host=localhost;dbname=shop;charset=utf8mb4';
-$user = 'staff';
-$dbPassword = 'password';
-$options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_EMULATE_PREPARES => false,
-];
-
 // 例外処理
 try {
     // データベース接続
-    $pdo = new PDO($dsn, $user, $dbPassword, $options);
+    $pdo = getDbConnection();
 
+    // カート商品削除
     // クエリ準備
     $sqlDeleteCart = '
     DELETE FROM cart
